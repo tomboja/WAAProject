58c63de99507108ae7bdac929fe53b5091579c96
TadeM <TadeM@users.noreply.github.com>
2021-12-11 22:41:46 -0600

    Spring Security

--- src/main/java/com/miu/waaproject/WaaProjectApplication.java
+++ src/main/java/com/miu/waaproject/WaaProjectApplication.java
@@ -1,13 +1,35 @@
 package com.miu.waaproject;
 
+import com.miu.waaproject.domain.User;
+import com.miu.waaproject.service.impl.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class WaaProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WaaProjectApplication.class, args);
+    }
+
+    @Bean
+    BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    CommandLineRunner run(UserService userService) {
+        return args -> {
+
+
+            userService.save(new User(null, "John Travolta", "john", "customer", "ADMIN"
+            ));
+        };
     }
 
 }
