66e960026491b624350f332844b29f118dad1a9e
Temesgen <temesgen.dessalegn@gmail.com>
2021-12-14 16:40:02 -0600

    User entity email unique added

--- src/main/java/com/miu/waaproject/domain/User.java
+++ src/main/java/com/miu/waaproject/domain/User.java
@@ -4,9 +4,8 @@ package com.miu.waaproject.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotNull;
 
 
 @Entity
@@ -17,11 +16,8 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    @NotNull
-    @Email
     @Column(unique = true)
     private String email;
-    @NotNull
     private String password;
     private String role;
 }
