638911d2b0fe4426626168df9d0150d8ce824c41
Temesgen <temesgen.dessalegn@gmail.com>
2021-12-16 16:38:23 -0600

    remove unneccessary files

--- frontend/src/components/RegistrationPage.js
+++ frontend/src/components/RegistrationPage.js
@@ -1,22 +1,6 @@
-import { createContext } from 'react'
-import Greeting from '../exam/Effect'
-
-const MyContext = createContext({ fName: 'Jason', lName: 'Gregory' })
-
 const RegistrationPage = (props) => {
-  const val = { fname: 'Georgina', lName: 'Tarek' }
   return (<div>
-    <h1>This is title</h1>
-    <MyContext.Provider value={val}>
-      <Greeting name='James' />
-      <Greeting name='saloo' />
-      <Greeting name='gobaa' />
-      <Greeting name='nilson' />
-    </MyContext.Provider>
-
   </div>)
 }
 
 export default RegistrationPage
-
-export { MyContext }
